48fe5b187ffe421c1c74bf33288511eaada25714
Gent (PG/R - Comp Sci & Elec Eng) <jiantao.wu@surrey.ac.uk>
2024-10-08 20:01:35 +0100

    reduce image size to 96

--- bin/exp.sh
+++ bin/exp.sh
@@ -2,6 +2,6 @@
 export WANDB_ENRITY=erow
 export WANDB_PROJECT=reproducibility
 
-WANDB_NAME=simclr_stl_e100 torchrun main_pretrain.py --config configs/simclr_stl.yml --data_path ~/gent/data --output_dir outputs/simclr_stl_e100 --epochs 100 
+WANDB_NAME=simclr_stl_e100 torchrun main_pretrain.py --config configs/simclr_stl.yml --data_path ~/data --output_dir outputs/simclr_stl_e100 --epochs 100 
 
-WANDB_NAME=simclr_stl_e200 torchrun main_pretrain.py --config configs/simclr_stl.yml --data_path ~/gent/data --output_dir outputs/simclr_stl_e200 --epochs 200
+WANDB_NAME=simclr_stl_e200 torchrun main_pretrain.py --config configs/simclr_stl.yml --data_path ~/data --output_dir outputs/simclr_stl_e200 --epochs 200
